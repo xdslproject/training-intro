e54c3fcd5d199c04cf450f91901e985299b082ca
Nick Brown <n.brown@epcc.ed.ac.uk>
2023-01-22 22:33:45 +0000

    Improved commenting and enhancements to parser and tiny_py dialect

--- practical/src/dialects/tiny_py.py
+++ practical/src/dialects/tiny_py.py
@@ -10,8 +10,18 @@ from xdsl.irdl import (AnyOf, AttributeDef, SingleBlockRegionDef, builder, Param
 from xdsl.parser import Parser
 from xdsl.printer import Printer
 
+"""
+This is our bespoke Python dialect that we are calling tiny_py. As you will see it is
+rather limited but is sufficient for our needs, and being simple means that we can easily
+navigate it and understand what is going on.
+"""
+
 @irdl_attr_definition
 class BoolAttr(Data[bool]):
+    """
+    Represents a boolean, MLIR does not by default have a boolean (it uses integer 1 and 0)
+    and-so this can be useful in your own dialects
+    """
     name = "bool"
     data: bool
 
@@ -34,10 +44,17 @@ class BoolAttr(Data[bool]):
 
 @irdl_attr_definition
 class EmptyAttr(ParametrizedAttribute):
+    """
+    This represents an empty value, can be useful where you
+    need a placeholder to explicitly denote that something is not filled
+    """
     name="empty"
 
 @irdl_op_definition
 class Module(Operation):
+    """
+    A Python module, this is the top level Python container which is a region
+    """
     name = "tiny_py.module"
 
     children = SingleBlockRegionDef()
@@ -45,16 +62,18 @@ class Module(Operation):
     @staticmethod
     def get(contents: List[Operation],
             verify_op: bool = True) -> FileContainer:
-      res = Module.build(regions=[contents])
-      if verify_op:
-        res.verify(verify_nested_ops=False)
-      return res
-
-    def verify_(self) -> None:
-      pass
+        res = Module.build(regions=[contents])
+        if verify_op:
+            res.verify(verify_nested_ops=False)
+        return res
 
 @irdl_op_definition
 class Function(Operation):
+    """
+    A Python function, our handling here is simplistic and limited but sufficient
+    for the exercise (and keeps this simple!) You can see how we have a mixture of
+    attributes and a region for the body
+    """
     name = "tiny_py.function"
 
     fn_name = AttributeDef(StringAttr)
@@ -64,36 +83,119 @@ class Function(Operation):
 
     @staticmethod
     def get(fn_name: Union[str, StringAttr],
-            return_var,
+            return_var: Union[Operation, None],
             args: List[Operation],
             body: List[Operation],
             verify_op: bool = True) -> Routine:
         if return_var is None:
-          return_var=EmptyToken()
-        res = Function.build(attributes={"fn_name": fn_name, "return_var": return_var, "args": ArrayAttr.from_list(args)},
-                            regions=[body])
+            # If return is None then use the empty token placeholder
+            return_var=EmptyAttr()
+        res = Function.build(attributes={"fn_name": fn_name, "return_var": return_var,
+                            "args": ArrayAttr.from_list(args)}, regions=[body])
         if verify_op:
             # We don't verify nested operations since they might have already been verified
             res.verify(verify_nested_ops=False)
         return res
 
-    def verify_(self) -> None:
-      pass
+@irdl_op_definition
+class Assign(Operation):
+    """
+    Represents variable assignment, where the LHS is the variable and RHS an expression. Note
+    that we are fairly limited here to representing one variable on the LHS only.
+    We also make life simpler by just storing the variable name as a string, rather than a reference
+    to the token which is also referenced directly by other parts of the code. The later is
+    more flexible, but adds additional complexity in the code so we keep it simple here.
+    """
+    name = "tiny_py.assign"
 
+    var_name = AttributeDef(StringAttr)
+    value = SingleBlockRegionDef()
 
-@irdl_attr_definition
-class Token(ParametrizedAttribute):
-    name = "tiny_py.token"
+    @staticmethod
+    def get(var_name: str,
+            value: Operation,
+            verify_op: bool = True) -> Assign:
+        res = Assign.build(attributes={"var_name":var_name}, regions=[[value]])
+        if verify_op:
+            # We don't verify nested operations since they might have already been verified
+            res.verify(verify_nested_ops=False)
+        return res
 
-    var_name : ParameterDef[StringAttr]
-    type : ParameterDef[AnyAttr()]
+@irdl_op_definition
+class Loop(Operation):
+    """
+    A Python loop, we take a restricted view here that the loop will operate on a variable
+    between two bounds (e.g. has been provided with a Python range).
+    
+    We have started this dialect definition off for you, and you will need to complete it.
+    There should be four members - a variable which is a string attribute and three
+    regions (the from and to expressions, and loop body)
+    """
+    name = "tiny_py.loop"
 
-@irdl_attr_definition
-class EmptyToken(EmptyAttr):
-    name = "tiny_py.emptytoken"
+    @staticmethod
+    def get(variable: str,
+            from_expr: Operation,
+            to_expr: Operation,
+            body: List[Operation],
+            verify_op: bool = True) -> If:
+        # We need to wrap from_expr and to_expr in lists because they are defined as separate regions
+        # and a region is a block with a list of operations. This is not needed for body because it is
+        # already a list of operations
+        res = Loop.build(attributes={"variable": variable}, regions=[[from_expr], [to_expr], body])
+        if verify_op:
+            # We don't verify nested operations since they might have already been verified
+            res.verify(verify_nested_ops=False)
+        return res
+
+@irdl_op_definition
+class Var(Operation):
+    """
+    A variable reference in Python, we just use the string name as storage here rather
+    than pointing to a token instance of the variable which others would also reference
+    directly.
+    """
+    name = "tiny_py.var"
+
+    variable = AttributeDef(StringAttr)
+
+    @staticmethod
+    def get(variable,
+            verify_op: bool = True) -> If:
+        res = Var.build(attributes={"variable": variable})
+        if verify_op:
+            # We don't verify nested operations since they might have already been verified
+            res.verify(verify_nested_ops=False)
+        return res
+
+@irdl_op_definition
+class BinaryOperation(Operation):
+    """
+    A Python binary operation, storing the operation type as a string
+    and the LHS and RHS expressions as regions
+    """
+    name = "tiny_py.binaryoperation"
+
+    op = AttributeDef(StringAttr)
+    lhs = SingleBlockRegionDef()
+    rhs = SingleBlockRegionDef()
+
+    @staticmethod
+    def get(op: str,
+            lhs: Operation,
+            rhs: Operation,
+            verify_op: bool = True) -> BinaryExpr:
+        res = BinaryOperation.build(attributes={"op": op}, regions=[[lhs], [rhs]])
+        if verify_op:
+            # We don't verify nested operations since they might have already been verified
+            res.verify(verify_nested_ops=False)
+        return res
 
 @irdl_op_definition
 class Constant(Operation):
+    """
+    A constant value, we currently support integers, floating points, and strings
+    """
     name = "tiny_py.constant"
 
     value = AttributeDef(AnyOf([StringAttr, IntegerAttr, FloatAttr]))
@@ -101,6 +203,7 @@ class Constant(Operation):
     @staticmethod
     def get(value: Union[None, bool, int, str, float], width=None,
             verify_op: bool = True) -> Literal:
+        if width is None: width=32
         if type(value) is int:
             attr = IntegerAttr.from_int_and_width(value, width)
         elif type(value) is float:
@@ -117,10 +220,21 @@ class Constant(Operation):
 
 @irdl_op_definition
 class Return(Operation):
+    """
+    Return from a function, we just support return without
+    any values/expressions at the moment
+    """
     name = "tiny_py.return"
 
 @irdl_op_definition
 class CallExpr(Operation):
+    """
+    Calling a function, in our example calling the print function, we store the target
+    function name and whether this is a builtin function as attributes (the second is
+    using the Boolean Attribute that we define in this dialect). The type of the call is
+    handled, as this is needed if the call is used as an expression rather than a statement,
+    and lastly the arguments to pass which are enclosed in a region.
+    """
     name = "tiny_py.call_expr"
 
     func = AttributeDef(StringAttr)
@@ -134,46 +248,32 @@ class CallExpr(Operation):
             type=EmptyAttr(),
             builtin=False,
             verify_op: bool = True) -> CallExpr:
+        # By default the type is empty attribute as the default is to call as a statement
         res = CallExpr.build(regions=[args], attributes={"func": func, "type": type, "builtin": builtin})
         if verify_op:
             # We don't verify nested operations since they might have already been verified
             res.verify(verify_nested_ops=False)
         return res
 
-    def verify_(self) -> None:
-      pass
-
 @dataclass
 class tinyPyIR:
     ctx: MLContext
 
     def __post_init__(self):
+        """
+        We need to register the attributes and operations defined in our dialect for parsing
+        so that xDSL can load in the text representation of the dialect and properly
+        structure it.
+        """
         self.ctx.register_attr(BoolAttr)
-        self.ctx.register_attr(Token)
-        self.ctx.register_attr(EmptyToken)
         self.ctx.register_attr(EmptyAttr)
 
         self.ctx.register_op(Module)
         self.ctx.register_op(Function)
         self.ctx.register_op(Return)
         self.ctx.register_op(Constant)
+        self.ctx.register_op(Assign)
+        self.ctx.register_op(Loop)
+        self.ctx.register_op(Var)
+        self.ctx.register_op(BinaryOperation)
         self.ctx.register_op(CallExpr)
-
-    @staticmethod
-    def get_type(annotation: str) -> Operation:
-        return TypeName.get(annotation)
-
-    @staticmethod
-    def get_statement_op_types() -> List[Type[Operation]]:
-        statements: List[Type[Operation]] = []
-        return statements + psyIR.get_expression_op_types()
-
-    @staticmethod
-    def get_expression_op_types() -> List[Type[Operation]]:
-        return [
-            CallExpr
-        ]
-
-    @staticmethod
-    def get_type_op_types() -> List[Type[Operation]]:
-        return []
--- practical/src/python_compiler.py
+++ practical/src/python_compiler.py
@@ -1,45 +1,166 @@
 import ast, inspect
 import tiny_py
 from xdsl.printer import Printer
+from xdsl.dialects.builtin import ModuleOp
 import sys
 
+"""
+This is our very simple Python parser which will parse the decorated
+function and generate an IR based on our tiny_py dialect. We use the Python
+ast library to do the parsing which keeps this simple. Note that there are
+other MLIR/xDSL Python parsers which are more complete, such as the xDSL
+frontend and pyMLIR
+"""
+
 def python_compile(func):
+    """
+    This is our decorator which will undertake the parsing and output the
+    xDSL format IR in our tiny_py dialect
+    """
     def compile_wrapper():
         a=ast.parse(inspect.getsource(func))
         analyzer = Analyzer()
         tiny_py_ir=analyzer.visit(a)
+        # This next line wraps our IR in the built in Module operation, this
+        # is required to comply with the MLIR standard (the top level must be
+        # a built in module).
+        tiny_py_ir=ModuleOp.from_region_or_ops([tiny_py_ir])
 
+        # Now we use the xDSL printer to output our built IR to stdio
         printer = Printer(stream=sys.stdout)
         printer.print_op(tiny_py_ir)
     return compile_wrapper
 
 class Analyzer(ast.NodeVisitor):
+    """
+    Our very simple Python parser based on the ast library. It's very simplistic but
+    provides an easy to understand view of how our IR is built up from the tiny_py
+    dialect that we have created for these practicals
+    """
     def generic_visit(self, node):
+        """
+        A catch all to print out the node if there is not an explicit handling function
+        provided
+        """
         print(node)
+        raise Exception("Unknown Python construct, no parser provided")
+
+    def visit_Assign(self, node):
+        """
+        Handle assignment, we visit the RHS and then create the tiny_py Assign IR operation
+        """
+        val=self.visit(node.value)
+        return tiny_py.Assign.get(node.targets[0].id, val)
 
     def visit_Module(self, node):
-        contents=[]
-        for a in node.body:
-          contents.append(self.visit(a))
-        return tiny_py.Module.get(contents)
-
-    def visit_FunctionDef(self, node):
+        """
+        Handles the top level Python module which contains many operations (here the
+        function that was decorated).
+        """
         contents=[]
         for a in node.body:
             contents.append(self.visit(a))
+        return tiny_py.Module.get(contents)
+
+    def visit_FunctionDef(self, node):
+        """
+        A Python function definition, note that we keep this simple by hard coding that
+        there is no return type and there are no arguments (it would be easy to extend
+        this to handle these and is left as an exercise).
+        """
+        contents=[]
+        for a in node.body:
+            operation=self.visit(a)
+            if operation is not None:
+                # We only need this check because we return None from our mocked out loop
+                # parser function that you will complete in exercise two, 
+                # so we don't want to include that in the operations
+                contents.append(operation)
         return tiny_py.Function.get(node.name, None, [], contents)
 
     def visit_Constant(self, node):
-      return tiny_py.Constant.get(node.value)
+        """
+        A literal constant value
+        """
+        return tiny_py.Constant.get(node.value)
+
+    def visit_Name(self, node):
+        """
+        Variable name
+        """
+        return tiny_py.Var.get(node.id)
+
+    def visit_For(self, node):
+        """
+        Handles a for loop, note that we make life simpler here by assuming that
+        it is in the format for i in range(from, to), and that is where we get
+        the from and to expressions.
+                
+        This function currently visits all the children in the loop body and 
+        appends their operations to the contents list. It also obtains the operations
+        that represent the from and to expressions.
+        """
+        contents=[]
+        for a in node.body:
+            contents.append(self.visit(a))
+        expr_from=self.visit(node.iter.args[0])
+        expr_to=self.visit(node.iter.args[1])
+        
+        # Now you need to construct the tiny_py Loop and return it
+        return None       
+
+    def visit_BinOp(self, node):
+        """
+        A binary operation
+        """
+        op_str=self.getOperationStr(node.op)
+        if op_str is None:
+            raise Exception("Operation "+str(node.op)+" not recognised")
+        lhs=self.visit(node.left)
+        rhs=self.visit(node.right)
+        return tiny_py.BinaryOperation.get(op_str, lhs, rhs)
 
     def visit_Call(self, node):
+        """
+        Calling a function, we provide a boolean describing whether this is a
+        built in Python function (e.g. print) or a user defined function.
+        """
         arguments=[]
         for arg in node.args:
             arguments.append(self.visit(arg))
-        builtin_fn=False
-        if node.func.id == "print":
-          builtin_fn=True
+        builtin_fn=self.isFnCallBuiltIn(node.func.id)
         return tiny_py.CallExpr.get(node.func.id, arguments, builtin=builtin_fn)
 
     def visit_Expr(self, node):
+        """
+        Visit a generic Python expression (it will then call other functions
+        in our parser depending on the expression type).
+        """
         return self.visit(node.value)
+
+    def isFnCallBuiltIn(self, fn):
+        """
+        Deduces whether a function is built in or not
+        """
+        if fn == "print":
+            return True
+        elif fn == "range":
+            return True
+
+        return False
+
+    def getOperationStr(self, op):
+        """
+        Maps Python operation to string name, as we use the string name
+        in the tiny_py dialect
+        """
+        if isinstance(op, ast.Add):
+            return "add"
+        elif isinstance(op, ast.Sub):
+            return "sub"
+        elif isinstance(op, ast.Mult):
+            return "mult"
+        elif isinstance(op, ast.Div):
+            return "div"
+        else:
+            return None
